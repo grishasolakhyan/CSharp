6005f015e566aa135701c5139fb8ca9e2fede181
grishasolakhyan <borsch.tv@mail.ru>
2022-11-14 16:16:44 +1000

    Hello, World!

--- Tasks/Задача-3/Задача-3/Справочник.cs
+++ Tasks/Задача-3/Задача-3/Справочник.cs
@@ -6,27 +6,28 @@ namespace Задача_3
     {
         static void Main(string[] args)
         {
-            int num;
-            Console.WriteLine("Введите номер задачи:" +
-                              "\n1)ЧТО ТАКОЕ ОПЕРАТОР IF-ELSE?" +
-                              "\n2)ЧТО ТАКОЕ ОПЕРАТОР SWITCH?" +
-                              "\n0)ЗАКРЫТЬ ЗАДАЧУ");
+            while (true)
+            {
+                int num;
+                Console.Write("1)ЧТО ТАКОЕ ОПЕРАТОР IF-ELSE?" +
+                                    "\n2)ЧТО ТАКОЕ ОПЕРАТОР SWITCH?" +
+                                    "\nВведите номер задачи: ");
                 num = (char)Console.Read();
                 switch (num)
                 {
                     case '1':
-                        Console.WriteLine("Оператор условия if-else служит для выбора\nнаправления работы программы в зависимости от\nусловий, сложившихся в данной точке программы на\nмомент ее выполнения.");
+                        Console.WriteLine("\nОператор условия if-else служит для выбора\nнаправления работы программы в зависимости от\nусловий, сложившихся в данной точке программы на\nмомент ее выполнения.");
                         break;
                     case '2':
-                        Console.WriteLine("Конструкция switch-case — это удобная замена\nдлинной if-else конструкции, которая сравнивает\nпеременную с несколькими константными\nзначениями, например int или char.");
-                        break;
-                    case '0':
+                        Console.WriteLine("\nКонструкция switch-case — это удобная замена\nдлинной if-else конструкции, которая сравнивает\nпеременную с несколькими константными\nзначениями, например int или char.");
                         break;
                     default:
-                        Console.WriteLine("Неверно выбран номер задачи");
+                        Console.WriteLine("\nНеверно выбран номер задачи");
                         break;
                 }
-                Console.ReadKey();
+                Console.WriteLine("\n");
+                Console.ReadLine();
+            }
         }
     }
 }
